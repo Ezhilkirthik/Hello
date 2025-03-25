585583b5d866c11f263f62f92b56874cf246bd56
Ezhilkirthik M <145484663+Ezhilkirthik@users.noreply.github.com>
2025-03-25 21:11:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 visitor_data = []
 
 # Replace with your Render.com endpoint
-RENDER_ENDPOINT = "https://hello-b8qv.onrender.com/log"
+RENDER_ENDPOINT = "https://hello-b8qv.onrender.com/track"
 
 @app.route('/track')
 def track():
